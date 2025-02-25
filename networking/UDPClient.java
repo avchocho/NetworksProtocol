f73d65d0d04e63887a8fbf0bb845c2afea6056ca
mmohr23 <116085310+mmohr23@users.noreply.github.com>
2025-02-24 23:55:55 -0500

    heartbeat

--- networking/UDPClient.java
+++ networking/UDPClient.java
@@ -2,6 +2,7 @@ package networking;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Random;
 /**
  * 
  * @author cjaiswal
@@ -9,51 +10,94 @@ import java.net.*;
  *  
  * 
  */
-public class UDPClient 
-{
+
+public class UDPClient {
     DatagramSocket Socket;
+    private InetAddress serverAddress;
+    private final int SERVER_PORT = 9876;
+    private final String NODE_ID = "Node1";
 
-    public UDPClient() 
-    {
-
-    }
-
-    public void createAndListenSocket() 
-    {
-        try 
-        {
+    public UDPClient() {
+        try {
             Socket = new DatagramSocket();
-            InetAddress IPAddress = InetAddress.getByName("localhost");
-            byte[] incomingData = new byte[1024];
-            String sentence = "Viehmann";
-            byte[] data = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, 9876);
-            Socket.send(sendPacket);
-            System.out.println("Message sent from client");
-            DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-            Socket.receive(incomingPacket);
-            String response = new String(incomingPacket.getData());
-            System.out.println("Response from server:" + response);
-            Socket.close();
-        }
-        catch (UnknownHostException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (SocketException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (IOException e) 
-        {
+            serverAddress = InetAddress.getByName("localhost");
+        } catch (SocketException | UnknownHostException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) 
-    {
+    public void sendHeartbeat() {
+        new Thread(() -> {
+            Random rand = new Random();
+            while (true) {
+                try {
+                    int sleepTime = rand.nextInt(31) * 1000;  // Random interval (0-30s)
+                    System.out.println("CLIENT waiting " + (sleepTime / 1000) + " sec before next heartbeat...");
+
+                    // Create the heartbeat message
+                    String heartbeatMessage = "HEARTBEAT|" + NODE_ID;
+                    byte[] data = heartbeatMessage.getBytes();
+
+                    // Send the heartbeat packet
+                    DatagramPacket sendPacket = new DatagramPacket(data, data.length, serverAddress, SERVER_PORT);
+                    Socket.send(sendPacket);
+                    System.out.println("CLIENT sent heartbeat: " + heartbeatMessage);
+
+                    // Wait before sending the next heartbeat
+                    Thread.sleep(sleepTime);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void createAndListenSocket() {
+        try {
+            sendHeartbeat(); // Start heartbeat messages
+
+            byte[] incomingData = new byte[1024];
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            Random rand = new Random();
+
+            while (true) {  // Continuous loop
+
+                System.out.println("");
+
+                int sleepTime = rand.nextInt(31) * 1000;  // Random interval (0-30s)
+
+                String sentence = "Viehmann";
+                byte[] data = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, 9876);
+                Socket.send(sendPacket);
+                System.out.println("SERVER message sent from client");
+
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+                Socket.receive(incomingPacket);
+                String response = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+                System.out.println("SERVER response from server: " + response);
+
+                Thread.sleep(sleepTime);
+            }
+        } 
+        catch (UnknownHostException e) {
+            e.printStackTrace();
+        } 
+        catch (SocketException e) {
+            e.printStackTrace();
+        } 
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } 
+        finally {
+            if (Socket != null && !Socket.isClosed()) {
+                Socket.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         UDPClient client = new UDPClient();
         client.createAndListenSocket();
     }
 }
-
--- networking/UDPServer.java
+++ networking/UDPServer.java
@@ -1,74 +1,74 @@
 package networking;
 
+/**
+* 
+* @author cjaiswal
+*
+*  
+* 
+*/
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-/**
- * 
- * @author cjaiswal
- *
- *  
- * 
- */
-public class UDPServer 
-{
+
+public class UDPServer {
     DatagramSocket socket = null;
 
-    public UDPServer() 
-    {
-
+    public UDPServer() {
+        // No changes here
     }
-    public void createAndListenSocket() 
-    {
-        try 
-        {
+
+    public void createAndListenSocket() {
+        try {
             socket = new DatagramSocket(9876);
             byte[] incomingData = new byte[1024];
 
-            while (true) 
-            {
-                DatagramPacket incomingPacket = new DatagramPacket(incomingData, 
-                		incomingData.length);
+            while (true) {
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
                 socket.receive(incomingPacket);
-                String message = new String(incomingPacket.getData());
+                String message = new String(incomingPacket.getData(), 0, incomingPacket.getLength()); // Trim message
                 InetAddress IPAddress = incomingPacket.getAddress();
                 int port = incomingPacket.getPort();
-                
+
                 System.out.println("Received message from client: " + message);
-                System.out.println("Client IP:"+IPAddress.getHostAddress());
-                System.out.println("Client port:"+port);
-                
+                System.out.println("Client IP: " + IPAddress.getHostAddress());
+                System.out.println("Client Port: " + port);
+
                 String reply = "Thank you for the message";
                 byte[] data = reply.getBytes();
-                
-                DatagramPacket replyPacket =
-                        new DatagramPacket(data, data.length, IPAddress, port);
-                
+
+                DatagramPacket replyPacket = new DatagramPacket(data, data.length, IPAddress, port);
                 socket.send(replyPacket);
-                Thread.sleep(2000);
-                socket.close();
+                
+                System.out.println("Sent response to client");
+                
+                Thread.sleep(2000); // Keep this if you need a delay
             }
         } 
-        catch (SocketException e) 
-        {
+        catch (SocketException e) {
             e.printStackTrace();
         } 
-        catch (IOException i) 
-        {
+        catch (IOException i) {
             i.printStackTrace();
         } 
-        catch (InterruptedException e) 
-        {
+        catch (InterruptedException e) {
             e.printStackTrace();
+        } 
+        finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+                System.out.println("Server socket closed.");
+            }
         }
     }
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         UDPServer server = new UDPServer();
         server.createAndListenSocket();
     }
 }
+
 
